b48c8b7624cefc0768cb574d15b3155468de0064
Arunmozhi <aruntheguy@gmail.com>
2011-09-24 23:57:36 +0530

    first hand code without any testing

--- geocode/geocode.py
+++ geocode/geocode.py
@@ -3,9 +3,34 @@ This file will take a text file containing the list of addresses to be geocoded 
 This is written to get lat-long for future GIS applications.
 '''
 
+'''
 # Example code
 from geopy import geocoders
 g = geocoders.Google()
 place, (lat, lng) = g.geocode("Chennai")
 print "%s: %f, %f" % (place, lat, lng)
+
+'''
+
+import sys
+
+from geopy import geocoders
+
+def geocode_file(filename):
+    ''' This is the main function '''
+    f = open(filename, 'r+')
+    o = open("output.txt", "w")
+    for place in f:
+        g = geocoders.Google()
+        pla, (lat,lng) = g.geocode(place)
+        o.write(pla.split(",")[0]+","+str(lat)+","+str(lng))
+    f.close()
+    o.close()
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        geocode_file(sys.argv[1])
+    else:
+        print "Error:"
+        print "File Usage: geocode.py input_file.txt"
 
