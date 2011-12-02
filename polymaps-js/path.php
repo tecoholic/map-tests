c1f08893eef4b5384e1c825a4bbe6fe39a170993
Arunmozhi <aruntheguy@gmail.com>
2011-12-02 23:22:56 +0530

    path.php returns LineString instead of points

--- polymaps-js/path.php
+++ polymaps-js/path.php
@@ -1,30 +1,42 @@
 <?php
 error_reporting(-1);
-// PHP script to split the csv file and return requested data as JSON
+
+if(isset($_GET["hour"])){ $hour = $_GET["hour"];} // For returning hour specific data
+
+// Variables and constants
 $file = "/home/teco/Projects/map-tests/polymaps-js/busroute.csv";
 $row = 1;
 $geo = array("type" => "FeatureCollection",
              "features" => array(),
          );
+$preLat = 0;
+$preLon = 0;
+
+// Parse the CSV file
 if (($handle = fopen($file, "r")) !== FALSE) {
     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
         $num = count($data);
         if($row != 1){
-            $lat = 0+$data[$num-2];
-            $lon = 0+$data[$num-1];
-            $feature = array("type" => "Feature",
-                            "geometry" => array(
-                                "type" => "Point",
-                                "coordinates" => array($lon, $lat)
-                            ),
-                            "properties" => array("speed" => 0+$data[1], "time" => $data[0])
+            if($row === 2){
+                $preLat += $data[$num-2];
+                $preLon += $data[$num-1];
+            }
+            else{
+                $lat = 0+$data[$num-2];
+                $lon = 0+$data[$num-1];
+                $feature = array("type" => "Feature",
+                                "geometry" => array(
+                                                    "type" => "LineString",
+                                                    "coordinates" => array(array($preLon, $preLat),array($lon, $lat))
+                                                    ),
+                                "properties" => array("speed" => 0+$data[1], "time" => $data[0])
                         );
-            array_push($geo["features"],$feature);
+                array_push($geo["features"],$feature);
+                $preLat = $lat; // reassign lat,lon for next segment
+                $preLon = $lon;
+            }
         }
-        else{
-            // array_push($geo["features"],array("lastField" => $data[$num-2]));
-        }
-        $row++;
+       $row++;
 
     }
     fclose($handle);
