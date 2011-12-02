da31a1a77e8e108860dab3707c98650b77be79bf
Arunmozhi <aruntheguy@gmail.com>
2011-12-03 00:15:48 +0530

    now scroll bar updates map with relevent data

--- polymaps-js/path.php
+++ polymaps-js/path.php
@@ -11,30 +11,38 @@ $geo = array("type" => "FeatureCollection",
          );
 $preLat = 0;
 $preLon = 0;
-
+$preset = False;
 // Parse the CSV file
 if (($handle = fopen($file, "r")) !== FALSE) {
     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
         $num = count($data);
         if($row != 1){
-            if($row === 2){
-                $preLat += $data[$num-2];
-                $preLon += $data[$num-1];
+
+            $time = date_parse($data[0]);
+            if($time["hour"] === 0+$hour){
+ 
+                // setting starting point
+                if(!$preset){
+                    $preLat += $data[$num-2];
+                    $preLon += $data[$num-1];
+                    $preset = True;
+                }
+                else{
+                    $lat = 0+$data[$num-2];
+                    $lon = 0+$data[$num-1];
+                    $feature = array("type" => "Feature",
+                                    "geometry" => array(
+                                                        "type" => "LineString",
+                                                        "coordinates" => array(array($preLon, $preLat),array($lon, $lat))
+                                                        ),
+                                    "properties" => array("speed" => 0+$data[1], "time" => $data[0])
+                            );
+                    array_push($geo["features"],$feature);
+                    $preLat = $lat; // reassign lat,lon for next segment
+                    $preLon = $lon;
+               }
             }
-            else{
-                $lat = 0+$data[$num-2];
-                $lon = 0+$data[$num-1];
-                $feature = array("type" => "Feature",
-                                "geometry" => array(
-                                                    "type" => "LineString",
-                                                    "coordinates" => array(array($preLon, $preLat),array($lon, $lat))
-                                                    ),
-                                "properties" => array("speed" => 0+$data[1], "time" => $data[0])
-                        );
-                array_push($geo["features"],$feature);
-                $preLat = $lat; // reassign lat,lon for next segment
-                $preLon = $lon;
-            }
+
         }
        $row++;
 
